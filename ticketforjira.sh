7d3e107bbc2aa9a3547e7387ac7eae00e673c3c3
vvhg1 <victor.hobe.gelting@gmail.com>
2024-12-22 15:38:52 +0100

    work in progress

--- ticketforjira.sh
+++ ticketforjira.sh
@@ -39,6 +39,8 @@ ticketforjira() {
         echo ""
         echo "-o, --options: Show all status options"
         echo ""
+        echo "-c, --current: Show all tickets in the current sprint only"
+        echo ""
         echo "Tickets can be searched by fuzzy searching"
         echo "Selecting a ticket will move on to the next step where you can manipulate the ticket"
         echo "The ticket can be moved to a different status column, or closed"
@@ -59,6 +61,9 @@ ticketforjira() {
         -l | --list)
             only_list=true
             ;;
+        -c | --current)
+            only_current=true
+            ;;
         -o | --options)
             all_options=true
             ;;
@@ -66,7 +71,21 @@ ticketforjira() {
             show_all=true
             only_list=true
             ;;
+        -lc | -cl)
+            only_current=true
+            only_list=true
+            ;;
+        -lac | -cla | -lca | -cal | -acl | -alc)
+            show_all=true
+            only_current=true
+            only_list=true
+            ;;
         -lo | -ol)
+            only_list=true
+            all_options=true
+            ;;
+        -lco | -loc | -clo | -col | -ocl | -olc)
+            only_current=true
             only_list=true
             all_options=true
             ;;
@@ -74,8 +93,19 @@ ticketforjira() {
             show_all=true
             all_options=true
             ;;
+        -aoc | -aco | -oac | -oca | -cao | -coa)
+            show_all=true
+            only_current=true
+            all_options=true
+            ;;
         -lao | -loa | -alo | -aol | -ola | -oal)
             show_all=true
+            only_list=true
+            all_options=true
+            ;;
+        -laco | -laoc | -loac | -loca | -lcao | -lcoa | -alco | -aloc | -aclo | -acol | -aocl | -aolc | -olac | -olca | -oalc | -oacl | -ocal | -ocla)
+            show_all=true
+            only_current=true
             only_list=true
             all_options=true
             ;;
@@ -90,7 +120,7 @@ ticketforjira() {
     # TODO: use the email switching script if it exists
     JIRA_EMAIL=$(git config user.email)
     # api token
-    API_TOKEN=$(pass jiraapi)
+    JIRA_API_TOKEN=$(pass jiraapi)
     # get the repo owner
     gh_name=$(git remote get-url origin | sed -e 's/.*github.com\///' -e 's/\/.*//')
     gh_name=${gh_name#*:}
@@ -106,7 +136,7 @@ ticketforjira() {
     URL="https://${REPOOWNER}.atlassian.net/rest/api/3/project/search"
     # Fetch project overview
     response=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$URL")
 
@@ -142,7 +172,7 @@ ticketforjira() {
 
     BOARDS_URL="https://${REPOOWNER}.atlassian.net/rest/agile/1.0/board"
     bresponse=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$BOARDS_URL")
 
@@ -157,7 +187,7 @@ ticketforjira() {
     # get the board columns
     COLUMNS_URL="https://${REPOOWNER}.atlassian.net/rest/agile/1.0/board/$board_id/configuration"
     columns_response=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$COLUMNS_URL")
 
@@ -173,7 +203,7 @@ ticketforjira() {
     # get the people on the project
     PEOPLE_URL="https://${REPOOWNER}.atlassian.net/rest/api/3/user/assignable/search?project=$project_id&startAt=0&maxResults=50"
     people_response=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$PEOPLE_URL")
 
@@ -190,9 +220,9 @@ ticketforjira() {
     ISSUES_URL="https://${REPOOWNER}.atlassian.net/rest/agile/1.0/board/$board_id/issue"
     # WARN: hardcoded maxResults
     maxResults=500
-    fields="id,key,summary,status,assignee,created,issuetype,labels,parent"
+    fields="id,key,summary,status,assignee,created,issuetype,labels,parent,sprint"
     issues_response=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$ISSUES_URL?startAt=0&maxResults=$maxResults&fields=$fields")
 
@@ -211,7 +241,12 @@ ticketforjira() {
         max_summary_length=60
     fi
     # now we need to get the fields we want
-    clean_issues=$(echo "$issues_response" | jq -r '.issues[] | "\(.id)\t\(.key)\t\(.fields.summary)\t\(.fields.status.name)\t\(.fields.assignee.displayName)\t\(.fields.created)\t\(.fields.issuetype.name)\t\(.fields.labels)\t\(.fields.parent.key)"')
+    if [ "$only_current" = true ]; then
+        # clean issues filtered for current sprint
+        clean_issues=$(echo "$issues_response" | jq -r '.issues[] | select(.fields.sprint != null) | "\(.id)\t\(.key)\t\(.fields.summary)\t\(.fields.status.name)\t\(.fields.assignee.displayName)\t\(.fields.created)\t\(.fields.issuetype.name)\t\(.fields.labels)\t\(.fields.parent.key)"')
+    else
+        clean_issues=$(echo "$issues_response" | jq -r '.issues[] | "\(.id)\t\(.key)\t\(.fields.summary)\t\(.fields.status.name)\t\(.fields.assignee.displayName)\t\(.fields.created)\t\(.fields.issuetype.name)\t\(.fields.labels)\t\(.fields.parent.key)"')
+    fi
     # clean up the missing fields
     clean_issues=$(echo "$clean_issues" | sed 's`\[\]`-`g' | sed 's`null`-`g' | sed 's`\[``g' | sed 's`\]``g')
     # format the date
@@ -280,7 +315,7 @@ ticketforjira() {
     # print the issue, first we get the full issue
     ISSUES_URL="https://${REPOOWNER}.atlassian.net/rest/api/3/issue/$issue_num"
     full_issue=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$ISSUES_URL")
 
@@ -297,9 +332,11 @@ ticketforjira() {
     full_issue_assignee=$(echo "$full_issue" | jq -r '.fields.assignee.displayName')
     full_issue_created=$(echo "$full_issue" | jq -r '.fields.created')
     full_issue_issuetype=$(echo "$full_issue" | jq -r '.fields.issuetype.name')
-    full_issue_labels=$(echo "$full_issue" | jq -r '.fields.labels')
+    # we need to get the labels into one line
+    full_issue_labels=$(echo "$full_issue" | jq -r '.fields.labels[]' | tr '\n' ' ')
     full_issue_parent=$(echo "$full_issue" | jq -r '.fields.parent.key')
     full_issue_description=$(echo "$full_issue" | jq -r '.fields.description')
+    full_issue_children=$(echo "$full_issue" | jq -r '.fields.subtasks[].key')
 
     # Process the JSON with jq
     formatted_text=$(echo "$full_issue_description" | jq -r '
@@ -339,14 +376,27 @@ ticketforjira() {
     printf '%s\n' "Assignee: $full_issue_assignee"
     printf '%s\n' "Issue Type: $full_issue_issuetype"
     printf '%s\n' "Labels: $full_issue_labels"
-    printf '%s\n' "Parent: $full_issue_parent"
+    if [[ -n "$full_issue_parent" ]]; then
+        printf '%s\n' "Parent: $full_issue_parent"
+    else
+        printf '%s\n' "Parent: -"
+    fi
+    if [[ -n "$full_issue_children" ]]; then
+        printf "Children:\n$full_issue_children"
+    else
+        printf "Children: -\n"
+    fi
     printf "\033[1;31m\nDescription:\033[0m\n"
-    echo -e "$formatted_text"
+    if [[ -n "$formatted_text" ]]; then
+        echo -e "$formatted_text"
+    else
+        echo -e "No description available"
+    fi
     # status options:
     # actions should be the possible transitions
     URL="https://${REPOOWNER}.atlassian.net/rest/api/3/issue/$issue_num/transitions"
     response=$(curl -s -X GET \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         "$URL")
 
@@ -380,24 +430,23 @@ null    null    Assign"
     #
     status=$(printf '%s\n' "${actions[@]}" | fzf --with-nth=3..)
     # TODO: rm print statement
-    echo "$status"
-    status_name=$(echo "$status" | awk -F '\t' '{print $3}')
+    status_id=$(echo "$status" | awk -F '    ' '{print $1}')
+    status_name=$(echo "$status" | awk -F '    ' '{print $3}')
 
-    if [ "$status_name" == "$current_column" ]; then
+    if [ "$status_name" == "$full_issue_status" ]; then
         echo "No change"
         return 0
     elif [ "$status_name" == "Close issue" ]; then
         check_yes_no_internal "Close issue $full_issue_key? [Y/n]: "
         # TODO: rm print statement
         echo "Closing issue $full_issue_key"
-        status_id=$(echo "$status" | awk -F '\t' '{print $1}')
         # TODO: rm print statement
         echo "status_id: $status_id"
         return 0
         # TODO: untested
         URL="https://${REPOOWNER}.atlassian.net/rest/api/3/issue/$issue_num/transitions"
         response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-            -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+            -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
             -H "Accept: application/json" \
             -d "{\"transition\":{\"id\":\"$status_id\"}}" \
             "$URL")
@@ -434,7 +483,7 @@ null    null    Assign"
         git checkout $branch_name
         return 0
     #  let the user choose the assignees
-    elif [ "$status" == "Assign" ]; then
+    elif [ "$status_name" == "Assign" ]; then
         #     # get people on the project
         # let the user enter the assignee in the command line, listen for enter
         assignee=$(printf '%s\n' "${people_names[@]}" | fzf)
@@ -452,7 +501,7 @@ null    null    Assign"
                 # TODO: untested
                 URL="https://${REPOOWNER}.atlassian.net/rest/api/3/issue/$issue_num/assignee"
                 response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-                    -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+                    -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
                     -H "Accept: application/json" \
                     -H "Content-Type: application/json" \
                     -d "{\"accountId\":\"$assignee_id\"}" \
@@ -474,7 +523,7 @@ null    null    Assign"
     fi
 
     # ----------------------good to here
-    if [ -z "$status" ]; then
+    if [ -z "$status_name" ]; then
         echo "No status selected"
         return 0
     # if the ticket goes into In Progress, we want to create a branch, but only if it's not a nocode or epic ticket
@@ -532,14 +581,12 @@ null    null    Assign"
         fi
     fi
     # move the ticket to the new status
-    echo "moving ticket to $status"
-    status_id=$(echo "$status" | awk -F '\t' '{print $1}')
-    echo "status_id: $status_id"
+    echo "moving ticket to $status_name"
     return 0
     # TODO: untested
     URL="https://${REPOOWNER}.atlassian.net/rest/api/3/issue/$issue_num/transitions"
     response=$(curl -s -o /dev/null -w "%{http_code}" -X POST \
-        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$API_TOKEN" | base64)" \
+        -H "Authorization: Basic $(echo -n "$JIRA_EMAIL:$JIRA_API_TOKEN" | base64)" \
         -H "Accept: application/json" \
         -d "{\"transition\":{\"id\":\"$status_id\"}}" \
         "$URL")
